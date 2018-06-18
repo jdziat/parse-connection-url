e9b87e074fc16653ce3179dd71635c0ac82bb429
Jordan Dziat <jdziat@gmail.com>
2018-06-18 09:55:39 -0700

    Cleaning up the tests

--- src/example.js
+++ src/example.js
@@ -16,7 +16,7 @@ const con = new Connector({
   'connection': {
     'prefix': '', 'hostname': 'sealws', 'port': 8081, 'path': '/seal-ws', 'secure': false, 'type': 'http', 'protocol': 'http'
   },
-  'auth': { 'username': 'root', 'password': '{enc3}0be3c7edcbbc24dd9083fcd47249a8c238de94aced171b54202f3a0db9ffe0af1318e6bdbda986fbe932b6be8d8ea8a5a10099887e091e41fc46cf6b522528509e525f4edf2e36e5172eb6f759dc3b387f8835985e1370cdfdc004d54fd92505aadd03e9f1a6c6d3e5aca78c4c77ac4e9d151302aca7926b13628afc1ec58df28e4f6d430a198944e7146faf969ba869' }
+  'auth': { 'username': 'root', 'password': 'test' }
 })
 console.log(con)
 // console.log(JSON.stringify(Util.parseUrl('jdbc:postgresql://postgres:5432/blast')))
